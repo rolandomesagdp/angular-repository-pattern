f06caf9ba38b98725797faba36ca12d3ab21709e
rmesafluidra <rmesa@fluidra.com>
2023-11-29 14:25:53 +0100

    Listener enhanced

--- projects/ui-pro/src/app/analysis-result-listener.ts
+++ projects/ui-pro/src/app/analysis-result-listener.ts
@@ -4,11 +4,11 @@ import { Observable, Subject, concatMap, map, takeUntil, tap, timer } from "rxjs
 export class AnalysisResultListener {
    private stop$: Subject<void> = new Subject<void>();
    private maxNumber: number = 5;
-   private timeTrack: number = 0;
+   timeTrack: number = 0;
 
    private timer$ = timer(0, 1000);
 
-   analysisResult$: Observable<string[]> = this.timer$.pipe(
+   analysisResult$: Observable<string[] | null> = this.timer$.pipe(
       tap(() => { this.timeTrack++}),
       concatMap(() => {
          if(this.timeTrack > this.maxNumber) {
@@ -17,10 +17,17 @@ export class AnalysisResultListener {
          return this.analysisService.getAll();
       }),
       map((analysis: string[]) => {
-         return [...analysis, this.timeTrack.toString()]
+         return this.validateResult(analysis);
       }),
       takeUntil(this.stop$)
    )
 
    constructor(private analysisService: AnalysisServiceContracts) { }
+
+   private validateResult(analysis: string[]): string[] | null {
+      if(this.timeTrack < this.maxNumber) {
+         return null;
+      }
+      else return analysis;
+   }
 }
