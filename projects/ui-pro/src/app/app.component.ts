0927a09eaf0aaa6ce5772d9593c6a913eb80fcda
rmesafluidra <rmesa@fluidra.com>
2023-11-29 10:13:51 +0100

    Repository pattern working with abstract class

--- projects/ui-pro/src/app/app.component.ts
+++ projects/ui-pro/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnalysisServiceContracts } from 'core';
 
 @Component({
    selector: 'app-root',
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
    title = 'ui-pro';
+   
+   items: string[] = ["one", "two"];
 
+   constructor(public analysisService: AnalysisServiceContracts) { }
 }
